071518cc1b5b25d0690d638b8f2b5ce64e3c3282
Martin Giger <martin@humanoids.be>
2015-11-24 17:02:41 +0100

    Clean up irc Handlers on upgrade

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const { Factory } = require("sdk/platform/xpcom");
 const { uuid } = require("sdk/util/uuid");
-
-//TODO test if uuid removes some of the reregistration trouble
+const self = require("sdk/self");
 
 const { CategoryEntry } = require("lib/category-manager");
 const { TwitchProtocol } = require("lib/twitch");
-
+const handlers = require("lib/twitch/handlers");
 
 const PROTOCOL_CATEGORY = "im-protocol-plugin";
 
 let protoInst = new TwitchProtocol();
+
+if(self.loadReason == "upgrade" || self.loadReason == "downgrade") {
+    //TODO delete category entry?
+    handlers.cleanUp();
+}
 
 const factory = Factory({
     contract: protoInst.contractID,
